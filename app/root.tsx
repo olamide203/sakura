31e5a6fc8fa5a71c4cf458de2bde1d3ab41e7e8e
Olamide Atitebi <olamideumarq@gmail.com>
2024-07-04 03:56:03 +0100

    feat: create hero svg

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
