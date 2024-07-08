8acd8aec23ea63647666734484a5303311077c1d
Olamide Atitebi <olamideumarq@gmail.com>
2024-07-08 01:53:33 +0100

    feat: better useMediaQuery hook

---
+++ app/hooks/useMediaQuery.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function useMediaQuery(mediaQueryString: string) {
+  const [matches, setMatches] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(mediaQueryString);
+    const listener = () => {
+      setMatches(!!mediaQueryList.matches);
+    };
+    listener();
+    mediaQueryList.addEventListener("change", listener);
+    return () => mediaQueryList.removeEventListener("change", listener);
+  }, [mediaQueryString]);
+  return matches;
+}
