948e9d5c20fd57e0413b52f503c39be5c0f901e7
Olamide Atitebi <olamideumarq@gmail.com>
2024-08-27 23:45:56 +0100

    fix: add studioHost to sanity.cli

--- packages/cms/sanity.cli.ts
+++ packages/cms/sanity.cli.ts
@@ -3,6 +3,7 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'k5cl2l8h',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  studioHost: 'odunola',
 })
